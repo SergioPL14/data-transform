4ad31dbad6b319d77c53d8e656da2ece28815b70
sergio_pina <sergio.pina@aktana.com>
2022-03-12 11:37:47 +0100

    working with parameters

--- datatransform/data_transform.py
+++ datatransform/data_transform.py
@@ -1,3 +1,5 @@
+import sys
+
 import numpy as np
 import pandas as pd
 from datetime import date, datetime
@@ -7,10 +9,12 @@ import os
 # Variable that contains the root directory
 ROOT_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__), '..')).replace('\\', '/')
 
-#Helper function to read config
+
+# Helper function to read config
 def read_config():
     # Opening JSON file
-    f = open(ROOT_DIR + '/inputs/clean_bookings.json')
+    # f = open(ROOT_DIR + '/inputs/clean_bookings.json')
+    f = open(sys.argv[1])
 
     # returns JSON object as
     # a dictionary
@@ -19,6 +23,7 @@ def read_config():
     # Closing file
     f.close()
     return data
+
 
 # Helper function to run the wanted transform depending on config file
 def exec_function(input_key, *params):
@@ -77,8 +82,9 @@ def fill_empty_values(df_in, col, value):
 # main function
 def main():
     print(ROOT_DIR)
+    print(sys.argv[1])
     config = read_config()
-    df = pd.read_csv(ROOT_DIR + '/' + config['source']['path'] + config['source']['dataset'] + '.' + config['source']['format'])
+    df = pd.read_csv(config['source']['path'] + '/' + config['source']['dataset'] + '.' + config['source']['format'])
 
     for i in range(0, len(config['transforms'])):
         for j in range(0, len(config['transforms'][i]['fields'])):
@@ -93,10 +99,11 @@ def main():
                               config['transforms'][i]['fields'][j]['value'])
 
     if config['sink']['format'] == 'jsonl':
-        df.to_json(ROOT_DIR + '/' + config['sink']['path'] + config['sink']['dataset'] + '.' + config['sink']['format'],
+        df.to_json(config['sink']['path'] + '/' + config['sink']['dataset'] + '.' + config['sink']['format'],
                    orient='records', lines=True)
     elif config['sink']['format'] == 'parquet':
-        df.to_parquet(ROOT_DIR + '/' + config['sink']['path'] + config['sink']['dataset'] + '.' + config['sink']['format'])
+        df.to_parquet(
+            config['sink']['path'] + '/' + config['sink']['dataset'] + '.' + config['sink']['format'])
 
 
 if __name__ == "__main__":
